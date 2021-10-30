90dd949c3261f8500c00fb4e53e6427630804723
unknown <adrianmu@kth.se>
2021-10-30 19:20:55 +0200

    did something that works even though the code is shit

---
+++ read_csv.py
@@ -0,0 +1,43 @@
+import csv
+
+english_words = ['meme', 'cookie', 'app', 'dumpling',
+    'emoticon', 'fashionista', 'selfie', 'smiley',
+    'nickname', 'quiz', 'vibe', 'incel', 'software',
+    'smartphone', 'remake', 'gigabyte', 'countdown',
+    'firewall', 'fidget spinner', 'youtuber', 'outfit',
+    'laptop', 'tablet', 'chat', 'viagra', 'smog',
+    'livestream', 'look', 'captcha', 'hackathon']
+
+japanese_words = ['torii', 'yukata', 'anime', 'izakaya',
+    'gyoza', 'kombucha', 'ikigai', 'akita', 'keiretsu',
+    'emoji', 'sudoku', 'tofu', 'haiku', 'netsuke',
+    'hikikomori', 'onsen', 'tanuki', 'sake', 'mochi', 'geisha']
+
+words = english_words + japanese_words
+answers = {}
+gender_to_int = {'en': 0, 'ett': 1}
+ett_count = {w:0 for w in words}
+
+# read csv and create dictionary with responses
+with open('data.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    column_start = 9
+    columns = len(words)
+
+    for row in csv_reader:
+        if line_count > 0:
+            answers[line_count] = {words[i-column_start]:gender_to_int[row[i]] for i in range(column_start, column_start+columns)}
+        line_count += 1
+
+for a in answers.values():
+    for b in a.keys():
+        ett_count[b] += a[b]
+ett_count = dict(sorted(ett_count.items(), key=lambda item: -item[1]))
+
+sorted_answers = {}
+
+for a in answers.keys():
+    sorted_answers[a] = [answers[a][w] for w in ett_count.keys()]
+
+sorted_answers = dict(sorted(sorted_answers.items(), key=lambda item: -item[1].count(gender_to_int['ett'])))
---
+++ visualize.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from matplotlib.pyplot import figure
+import numpy as np
+from matplotlib import colors
+from read_csv import *
+
+data = np.ma.array([answer for answer in sorted_answers.values()]).transpose()
+
+# create discrete colormap
+cmap = colors.ListedColormap(['lightgrey', 'dimgrey'])
+# 0/en is black, 1/ett is gray
+bounds = [-0.1, 0.5, 1.1]
+norm = colors.BoundaryNorm(bounds, cmap.N)
+
+fig, ax = plt.subplots()
+fig.set_size_inches(15, 7)
+ax.imshow(data, cmap=cmap, norm=norm)
+
+# draw gridlines
+ax.grid(which="minor")
+ax.tick_params(which="minor", size=0)
+ax.set_yticks(range(len(words)));
+ax.set_yticklabels(list(ett_count.keys()), rotation='horizontal', fontsize=8)
+ax.set_xticks(range(len(sorted_answers)));
+ax.set_xticklabels(list(sorted_answers.keys()), rotation='vertical', fontsize=6)
+ax.set_xlabel('respondent')
+ax.set_ylabel('borrowing')
+
+lightgrey_patch = mpatches.Patch(color='lightgrey', label='en')
+darkgrey_patch = mpatches.Patch(color='darkgrey', label='ett')
+plt.legend(handles=[lightgrey_patch, darkgrey_patch], loc="lower right")
+
+plt.show()
